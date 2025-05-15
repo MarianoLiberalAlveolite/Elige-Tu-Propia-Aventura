0c10d071d61b32d4c9c55ef12faab438acff072d
Jose Antonio del Pino <jadelpino10@gmail.com>
2025-05-15 14:40:03 +0200

    ADD: Diseño y funcinalidad en Main1. + Falta retocar los tonos de los gráficos

--- app/src/main/java/com/example/eligetupropiaaventuraapp/MainActivity.kt
+++ app/src/main/java/com/example/eligetupropiaaventuraapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.eligetupropiaaventuraapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main1)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val boton = findViewById<ImageButton>(R.id.btnGoToForm)
+        boton.setOnClickListener{
+            val intent = Intent(this, FormActivity::class.java)
+            startActivity(intent)
         }
     }
 }
